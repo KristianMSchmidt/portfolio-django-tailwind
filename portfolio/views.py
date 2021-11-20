874dc5e914979a440ae9cce8770fc01cb4fb5874
Kristian Møller Schmidt <kmollerschmidt@gmail.com>
2021-11-20 15:50:53 +0100

    fixed message bug

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,6 +6,7 @@ from django.http import Http404
 from .context import context as ctx
 from .forms import ContactForm
 from .models import Mail
+from django.contrib import messages
 
 
 def home_view(request):
@@ -14,21 +15,26 @@ def home_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            sender = form.cleaned_data['sender']
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-            recipients = ['kmollerschmidt@gmail.com', 'krms@kvuc.dk']
-            full_message = f"Portfolio message from {name} with email {sender}: {message}"
-            Mail.objects.create(name=name, sender=sender,
-                                subject=subject, message=message)
-            try:
-                send_mail(subject, full_message, sender,
-                          recipients, fail_silently=False)
-                context['email_status'] = 'success'
-                return redirect(reverse('home'), context)
-            except:
-                context['email_status'] = 'failure'
+
+            # name = form.cleaned_data['name']
+            # sender = form.cleaned_data['sender']
+            # subject = form.cleaned_data['subject']
+            # message = form.cleaned_data['message']
+            # recipients = ['kmollerschmidt@gmail.com', 'krms@kvuc.dk']
+            # full_message = f"Portfolio message from {name} with email {sender}: {message}"
+            # Mail.objects.create(name=name, sender=sender,
+            #                     subject=subject, message=message)
+            # try:
+            #     send_mail(subject, full_message, sender,
+            #               recipients, fail_silently=False)
+            #     messages.success(
+            #         request, "Your message was send. I'll reply as soon as possible.")
+            #     return redirect(reverse('home'))
+            # except:
+            #     messages.error(
+            #         request, 'Your message was not send. Try again or send a regular email to the address shown below.')
+            messages.error(
+                request, 'There was an error. Please send your message to kmollerschmidt@gmail.com.')
     else:
         form = ContactForm()
 
